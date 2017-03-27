07ab4e731f9abc550aec713e3ee2c76ccd1c56a4
Dmitriy Gajewski <dmit8815@gmail.com>
2017-03-27 08:52:29 +0300

    code style fix

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-"use strict";
-let exec = require("child_process").exec;
-let config = require("./config.json");
-let https = require("https");
+const exec = require("child_process").exec;
+const config = require("./config.json");
+const https = require("https");
+
 function getSelection() {
     return new Promise(function (resolve, reject) {
         exec("/usr/bin/xclip -o", function(err, stdout, stderr) {
@@ -14,27 +14,28 @@ function getSelection() {
 }
 
 getSelection().then(function (stdout) {
-    //let args = require("minimist")(process.argv.slice(2));
     https.get({
         hostname: "translate.yandex.net",
         port: 443,
         path: `/api/v1.5/tr.json/translate?key=${config.apiKey}&lang=en-ru&text=${encodeURIComponent(stdout)}`,
         agent: false
-    }, (res) => {
+    }, res => {
         let rawData = "";
-        res.on("data", (chunk) => rawData += chunk);
+
+        res.on("data", chunk => rawData += chunk);
         res.on("end", () => {
             try {
-                let parsedData = JSON.parse(rawData);
+                const parsedData = JSON.parse(rawData);
+
                 exec(`/usr/bin/notify-send "Yandex Translate" "${parsedData.text}"`, function(err, stdout, stderr) {
                     if (err || stderr) {
-                        console.log(err || stderr);
+                        console.error(err || stderr);
                     }
                 });
             } catch (e) {
                 exec(`/usr/bin/notify-send "Yandex Translate" "${e.message}"`, function(err, stdout, stderr) {
                     if (err || stderr) {
-                        console.log(err || stderr);
+                        console.error(err || stderr);
                     }
                 });
             }
